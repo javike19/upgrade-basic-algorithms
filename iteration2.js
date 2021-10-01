29bfe219b2139f02f54aaf19684a026c327f186e
unknown <javimorenomontejo@gmail.com>
2021-10-01 17:16:43 +0200

    Iteration#2

--- iteration2.js
+++ iteration2.js
@@ -1,6 +1,6 @@
 //#2 Variables avanzadas
 //1.1
-/*const character = {name: 'Jack Sparrow', age: 10};
+const character = {name: 'Jack Sparrow', age: 10};
 
 age = 25;
 
@@ -10,13 +10,13 @@ let firstName = 'Jon';
 let lastName = 'Snow'; 
 let age = 24; 
 
-console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.");*/
+console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.");
 
 //1.3
-/*const toy1 = {name: 'Buss myYear', price: 19};
+const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 
-console.log(toy1.price + toy2.price);*/
+console.log(toy1.price + toy2.price);
 
 
 //1.4
